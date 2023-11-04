01fd0bdda549d2b044f28135504e4cdf40690e3a
unknown <gianni.carlini@sansano.usm.cl>
2023-11-04 19:29:01 -0300

    add solution to challenge 1

--- Challenge01/solucion.py
+++ Challenge01/solucion.py
@@ -1,2 +1,15 @@
-strs = open("message_01.txt")
-print(strs)
+file = open("message_01.txt", mode="r")
+data = file.read()
+
+splitData = data.rstrip('\n').split()
+counter = {}
+
+for animal in splitData:
+    if animal not in counter:
+        counter[animal] = 0
+    counter[animal] += 1
+
+res = ''
+for animalType, total in counter.items():
+    res += animalType + str(total)
+print(res)
