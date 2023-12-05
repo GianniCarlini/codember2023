e8d05dc07aa228e655cd8312e0a83787da28286c
unknown <gianni.carlini@sansano.usm.cl>
2023-12-05 11:34:03 -0300

    add solution to challenge 4

--- Challenge04/solucion.py
+++ Challenge04/solucion.py
@@ -1,17 +1,22 @@
 file = open("files_quarantine.txt", mode="rt")
-data = file.read().strip()
+data = file.read()
 
-result = 0
 data_formatted = data.split("\n");
-for line in data_formatted:
-    print(data_formatted)
-    # policy, password = line.split(":")
-    # policy, letter = policy.split(" "); 
-    # min, max = policy.split("-"); 
-    # count = password.count(letter)
-    # if count < int(min) or count > int(max):
-    #      result+=1
+checked_files = []
 
-    # if result == 42:
-    #     print(password)
-    #     break 
+for line in data_formatted:
+    chain, checksum = line.split("-")
+
+    check = ""
+    for letra in chain:
+      total = chain.count(letra)
+
+      if (total == 1):
+        check += letra
+
+    if (check == checksum):
+      checked_files.append(checksum)
+
+    if len(checked_files) > 32:
+        print(checked_files[32])
+        break 
