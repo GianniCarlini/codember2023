e9fbe1e8f251a5d80b8f934c69c0b445ed428d3d
Gianni Carlini <65686100+GianniCarlini@users.noreply.github.com>
2023-12-05 11:21:22 -0300

    base for challenge 4

---
+++ Challenge04/solucion.py
@@ -0,0 +1,17 @@
+file = open("files_quarantine.txt", mode="rt")
+data = file.read().strip()
+
+result = 0
+data_formatted = data.split("\n");
+for line in data_formatted:
+    print(data_formatted)
+    # policy, password = line.split(":")
+    # policy, letter = policy.split(" "); 
+    # min, max = policy.split("-"); 
+    # count = password.count(letter)
+    # if count < int(min) or count > int(max):
+    #      result+=1
+
+    # if result == 42:
+    #     print(password)
+    #     break 
